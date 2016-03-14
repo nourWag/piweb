9e6841817ce48bb6d7d5126306201ec9476de1e0
salma chhoud <salma.chhoud@esprit.tn>
2016-03-14 11:12:18 +0100

    Update DefaultControllerTest.php

--- src/piweb/appBundle/Tests/Controller/DefaultControllerTest.php
+++ src/piweb/appBundle/Tests/Controller/DefaultControllerTest.php
@@ -13,5 +13,6 @@ class DefaultControllerTest extends WebTestCase
         $crawler = $client->request('GET', '/hello/Fabien');
 
         $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        
     }
 }
